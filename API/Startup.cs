e46cf06486d4f44ff5ffe97e937d24001c2d83ff
Christopher Sayon <Christopher.L.Sayon@gmail.com>
2020-10-10 12:46:22 +1100

    Angular hosted with ssl

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,7 +54,8 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
